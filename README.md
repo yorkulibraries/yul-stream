09c23d72353fb90a6a9084d62b54f644edabb132
nruest <ruestn@gmail.com>
2012-09-26 13:32:27 -0400

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+York University Libraries Open Access Week twitter stream
+==========
+
+YUL Stream is a [Node](http://nodejs.org) web application that streams Tweets that reference the hashtag for the 2012 York University Libraries Open Access Week Debate.
+
+The application is inspired by, and heavily uses the code Ed Summers wrote in the wonderful [wikitweets](https://github.com/edsu/wikitweets) application.
+
+
+Thanks
+------
+
+YUIL Stream stands on several giants shoulders, including:
+
+* [express](http://expressjs.com/)
+* [socket.io](http://socket.io)
+* [ntwitter](https://github.com/AvianFlu/ntwitter)
+* [request](https://github.com/mikeal/request)
+* [unshorten](https://github.com/mathiasbynens/node-unshorten)
+* [underscore](http://documentcloud.github.com/underscore/)
+* [wikitweet](https://github.com/edsu/wikitweets)
+
+Install
+-------
+
+* install node and git
+* `git clone https://github.com/edsu/wikistream.git`
+* `cd wikistream`
+* `npm install`
+* `cp config.json.tmpl config.json`
+* get [Twitter](https://dev.twitter.com/apps/new) credentials and add them to 
+  config.json
+  wikitweets can archive JSON there.
+* `node app.js`
+* open http://localhost:3000/ in browser
+
+Licnse
+------
+
+![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png "CC0")
---
+++ app.js
@@ -0,0 +1,102 @@
+var fs = require('fs'),
+    path = require('path'),
+    http = require('http'),
+    jsdom = require('jsdom'),
+    _  = require('underscore'),
+    express = require('express'),
+    request = require('request'),
+    twitter = require('ntwitter'),
+    socketio = require('socket.io'),
+    unshorten = require('unshorten'),
+    dateformat = require('dateformat'),
+    querystring = require('querystring');
+
+var config = getConfig();
+var latest = [];
+
+function main() {
+  var sockets = [];
+
+  var app = express();
+  var server = http.createServer(app);
+  var io = socketio.listen(server);
+
+  app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+  });
+
+  // heroku specific configuration
+  io.configure('production', function() {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+  });
+
+  io.sockets.on('connection', function(socket) {
+    // don't send all of the latest tweets it can cause a lag
+    _.each(latest.slice(-10), function(t) {socket.emit('tweet', t);});
+    sockets.push(socket);
+    socket.on('disconnect', function() {
+      sockets = _.without(sockets, socket);
+    });
+  });
+
+  var tweets = new twitter(getConfig());
+  tweets.stream('statuses/filter', {track: '#yolo'}, function(stream) {
+    stream.on('data', function(t) {
+      console.log(t)
+      tweet(t, sockets);
+    });
+    stream.on('error', function(err, code) {
+      console.log('uhoh got a twitter stream error: ' + err + ' ; ' + code);
+    });
+    stream.on('limit', function(l) {
+      console.log('whoops. we got limited by twitter: ' + l);
+    });
+  });
+
+  server.listen(process.env.PORT || config.port || 3000);
+}
+
+function getConfig() {
+  // looks in a json file or the environment for some config info
+  try {
+    var configPath = path.join(__dirname, "config.json");
+    return JSON.parse(fs.readFileSync(configPath));
+  } catch(err) {
+    var config = {
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    }
+    console.log(config);
+    return config;
+  }
+}
+
+function tweet(t, sockets) {
+  var tweetUrl = "http://twitter.com/" + t.user.screen_name + "/statuses/" + t.id_str;
+  var msg = {
+    "id": t.id_str,
+    "url": tweetUrl,
+    "text": t.text,
+    "user": t.user.screen_name,
+    "name": t.user.name,
+    "avatar": t.user.profile_image_url,
+    "created": t.created_at,
+    "tweet": t,
+  };
+  addLatest(msg);
+  _.each(sockets, function(socket) {
+  socket.emit('tweet', msg);
+  });
+}
+
+function addLatest(msg) {
+  latest.push(msg);
+}
+
+if (! module.parent) {
+  main();
+}
+
